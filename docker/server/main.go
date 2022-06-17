81cbbdcc7306215edae5b829071a4accbcdc3ddc
WillIWas123 <william.kristof@gmail.com>
2022-06-18 01:44:52 +0200

    Fixed up the db, fixed up the extension in burp, more flexibility, more control over projects

--- docker/server/main.go
+++ docker/server/main.go
@@ -6,8 +6,6 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"crypto/md5"
-	"encoding/hex"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/websocket"
@@ -20,21 +18,29 @@ var db *sql.DB
 type wsmessage struct {
 	Action  string `json:"action"`
 	User    string `json:"user"`
-	Path    string `json:"path"`
+	Value    string `json:"value"`
+	Type string `json:"type"`
 	Color   string `json:"color"`
 	Comment string `json:"comment"`
-	Project string `json:"project"`
+	ProjectId int `json:"projectid"`
+	ProjectName string `json:"projectname"`
 }
+
+type list struct{
+	Name string `json:"name"`
+}
+
+
 
 type client struct {
 	con     *websocket.Conn
 	user    string
-	project string
+	projectId int
 }
 
-func removeClient(user string, project string) {
+func removeClient(user string, projectId int) {
 	for i := 0; i < len(clients); i++ {
-		if clients[i].user == user && clients[i].project == project {
+		if clients[i].user == user && clients[i].projectId == projectId {
 			clients[i] = clients[len(clients)-1]
 			clients = clients[:len(clients)-1]
 		}
@@ -53,51 +59,90 @@ func highlightSharing(w http.ResponseWriter, r *http.Request) {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
-			return
+			continue
 		}
 		var msg wsmessage
-
 		json.Unmarshal(message, &msg)
+		results, err := db.Query("SELECT id from project where name=(?);", &msg.ProjectName)
+		for results.Next(){
+			err = results.Scan(&msg.ProjectId)
+			if err != nil{
+				log.Println("Error in position 0")
+				log.Println(err)
+				continue
+                        }
+		}
 		if msg.Action == "UPDATE" {
-			defer removeClient(msg.User, msg.Project)
-			client := &client{con: c, user: msg.User, project: msg.Project}
+			var message wsmessage
+			defer removeClient(msg.User, msg.ProjectId)
+			client := &client{con: c, user: msg.User, projectId: msg.ProjectId}
 			clients = append(clients, client)
-			results, err := db.Query("SELECT path, color, comment, user from request where projectName = (?) AND user != (?)", msg.Project, msg.User)
-			if err != nil {
-				log.Println("Error in position 1")
+			results, err = db.Query("SELECT color, comment, user, type, value from request where user != (?) and projectId = (?);", msg.User, msg.ProjectId)
+			if results != nil{
+				for results.Next(){
+					err = results.Scan(&message.Color, &message.Comment, &message.User, &message.Type, &message.Value)
+					if err != nil{
+						log.Println("Error in position 1")
+						log.Println(err)
+						continue
+					}
+					message.ProjectName = msg.ProjectName
+					content, err := json.Marshal(message)
+					if err != nil {
+						log.Println("Error in position 3")
+						log.Println(err)
+						continue
+					}
+					client.con.WriteMessage(mt, content)
+				}
+				results.Close()
+			}
+		} else if msg.Action == "CREATE"{
+			_, err := db.Query("INSERT into project (name) values (?)", msg.ProjectName)
+			if err != nil{
+				log.Println("Error in position 3.5")
 				log.Println(err)
 				continue
 			}
-			for results.Next() {
-				var message wsmessage
-				err = results.Scan(&message.Path, &message.Color, &message.Comment, &message.User)
-				if err != nil {
-					log.Println("Error in position 2")
-					log.Println(err)
-					continue
-				}
-				message.Project = msg.Project
-				content, err := json.Marshal(message)
-				if err != nil {
-					log.Println("Error in position 3")
-					log.Println(err)
-					continue
-				}
-				client.con.WriteMessage(mt, content)
+		} else if msg.Action == "DELETE"{
+			_, err := db.Query("DELETE FROM project where name = (?)", msg.ProjectName)
+			if err != nil{
+				log.Println("Error in position 3.7")
+				log.Println(err)
+				continue
 			}
-			results.Close()
-		} else {
-			hash := md5.Sum([]byte(msg.Path))
-			md5sum := hex.EncodeToString(hash[:])
-			insert, err := db.Query("INSERT into request (user, projectName, path, color, comment, md5sum) VALUES (?,?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE user=(?), projectName=(?), path=(?), color=(?), comment=(?), md5sum=(?);", msg.User, msg.Project, msg.Path, msg.Color, msg.Comment, md5sum, msg.User, msg.Project, msg.Path, msg.Color, msg.Comment, md5sum)
+		} else if msg.Action == "LIST"{
+			var list list
+			results, err = db.Query("SELECT name from project;")
+			if results != nil{
+				for results.Next(){
+					err = results.Scan(&list.Name)
+					if err != nil{
+						log.Println("Error in position 4")
+						log.Println(err)
+						continue
+					}
+					content, err := json.Marshal(list)
+					if err != nil{
+						log.Println("Error in position 5")
+						log.Println(err)
+						continue
+					}
+					c.WriteMessage(mt, content)
+				}
+				results.Close()
+			}
+
+		} else{
+			insert, err := db.Query("INSERT into request (user, projectId, type, value, color, comment) VALUES (?,?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE user=(?), projectId=(?), type=(?), value=(?), color=(?), comment=(?);", msg.User, msg.ProjectId, msg.Type,msg.Value, msg.Color, msg.Comment, msg.User, msg.ProjectId, msg.Type, msg.Value, msg.Color, msg.Comment) // Could probably be handled better
 			if err != nil {
-				log.Println("Error in position 4")
+				log.Println("Error in position 7")
 				log.Println(err)
 				continue
 			}
 			insert.Close()
 			for i := 0; i < len(clients); i++ {
-				if clients[i].user != msg.User && clients[i].project == msg.Project {
+				if clients[i].user != msg.User && clients[i].projectId == msg.ProjectId {
 					clients[i].con.WriteMessage(mt, message)
 				}
 			}
@@ -121,11 +166,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	create, err := db.Query("CREATE TABLE IF NOT EXISTS request (id int not null auto_increment, projectName varchar(50) not null, path varchar(2048) not null, md5sum varchar(32) not null, color varchar(10) not null,comment varchar(255) not null,user varchar(20) not null,primary key(id), CONSTRAINT unique_key UNIQUE(md5sum,projectName));")
-	if err != nil {
-		log.Fatal(err)
-	}
-	create.Close()
+	create, err := db.Query("CREATE TABLE IF NOT EXISTS project (id int not null auto_increment, name varchar(50) not null unique,primary key(id));")
+        if err != nil {
+                log.Fatal(err)
+        }
+        create.Close()
+        create, err = db.Query("CREATE TABLE IF NOT EXISTS request (id int not null auto_increment, projectId int not null, type varchar(50) not null, value varchar(2048) not null, color varchar(10) not null, comment varchar(255) not null, user varchar(20) not null,primary key(id), CONSTRAINT unique_key UNIQUE (type, value)) CHARSET=latin1;") // chose latin1 for now, will have to figure out a solution to the length problem later 
+        if err != nil {
+                log.Fatal(err)
+        }
+        create.Close()
 	clients = []*client{}
 	http.HandleFunc("/", highlightSharing)
 	log.Fatal(http.ListenAndServe(host, nil))
--- highlightSharing.py
+++ highlightSharing.py
@@ -1,7 +1,7 @@
 from burp import IBurpExtender, ITab
 import time
 from burp import IContextMenuFactory, IBurpExtenderCallbacks, IRequestInfo,IExtensionStateListener
-from javax.swing import JPanel, JLabel, JTextField, SwingConstants, JButton
+from javax.swing import JPanel, JLabel, JTextField, SwingConstants, JButton,JList, JCheckBox
 from com.ziclix.python.sql import zxJDBC
 from java.net.http import HttpClient;
 from java.net.http import WebSocket
@@ -14,18 +14,24 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
 
     def __init__(self):
         self.debugging=False
-        self.version="v1.0"
+        self.version="v2.0"
+        self._scheme = True
+        self._domain = True
+        self._path = True
+        self._query = False
+        self._projectNames = ()
+        self._websocket=None
 
     ##### WEBSOCKET PART ######
 
     def onOpen(self, websocket):
         self._websocket=websocket
-        data={"action":"UPDATE", "user":self._user, "project":self._projectName}
+        data={"action":"LIST"}
         if not self.sendMessage(json.dumps(data)):
             self.writeText("Not connected: Failed to connect to the websocket server")
         self._websocket.request(1)
         self.writeText("Connected")
-        self._callbacks.printOutput("Connected to Burp-Highlight-Sharing "+ self.version)
+        self._projectNames = ()
 
     def onClose(self, websocket, statusCode, reason):
         self.writeText("Not connected: connection closed")
@@ -35,14 +41,20 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
             self._callbacks.printOutput("Received message")
         self._websocket=websocket
         content = json.loads(data.toString())
+        if "name" in content.keys():#updating list before choosing a project
+            self._projectNames+=(content["name"],)
+            self._projectList.setListData(self._projectNames)
+            self._websocket.request(1)
+            return
+
         user = content["user"]
-        project = content["project"]
-        if user != self._user and project == self._projectName:
+        project = content["projectname"]
+        if user != self._user and project == self._projectName and content["value"] != "":
             if self.debugging:
-                self._callbacks.printOutput("Updating path "+content["path"])
-            self.updateReq(content["path"], content["color"], content["comment"])
+                self._callbacks.printOutput("Updating URL: "+content["value"])
+            self.updateReq(content["type"], content["value"], content["color"], content["comment"])
         elif self.debugging:
-            self._callbacks.printOutput("NOT UPDATING "+content["path"])
+            self._callbacks.printOutput("NOT UPDATING URL: "+content["value"])
         self._websocket.request(1)
 
     def sendMessage(self, text):
@@ -58,26 +70,46 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
 
 
     def connectWebsocket(self, event):
+        if self.debugging:
+            self._callbacks.printOutput("connecting")
         self.writeText("Not connected")
         self._wsString = self._wsStringButton.getText()
-        self._projectName = self._projectButton.getText()
         self._user = self._userButton.getText()
-        if self._projectName =="" or self._user == "":
+        if self._wsString == "" or self._user == "":
             self.writeText("Not connected: Please specify a username and ws string")
             return
         client = HttpClient.newHttpClient()
-        client.newWebSocketBuilder().buildAsync(URI.create(self._wsString),  self);
+        client.newWebSocketBuilder().buildAsync(URI.create(self._wsString),  self)
+        
 
-    def toggleDebug(self, event):
-        self.debugging = not self.debugging
-        if self.debugging:
-            self._messageBox.text+=" and debugging enabled"
 
     def writeText(self, connection):
         if self.debugging:
             self._messageBox.text=connection+" and debugging enabled"
         else:
             self._messageBox.text=connection
+
+    def selectProject(self, var2):
+        self._projectName = self._projectList.getSelectedValue()
+        if self.debugging:
+            self._callbacks.printOutput("Selected project: "+self._projectName)
+        data={"action":"UPDATE", "user":self._user, "projectname":self._projectName}
+        if not self.sendMessage(json.dumps(data)):
+            self.writeText("Failed to update project")
+        self._websocket.request(1)
+
+    def createProject(self, var2):
+        data = {"action":"CREATE", "projectname":self._newProject.getText()}
+        if not self.sendMessage(json.dumps(data)):
+            self.writeText("Failed to create project")
+        self._projectName = self._newProject
+        self._websocket.request(1)
+
+    def deleteProject(self, var2):
+        data = {"action":"DELETE", "projectname":self._projectList.getSelectedValue()} # fyi: will not actually delete any requests from the db, but the project is deleted and will not be visible in burp
+        if not self.sendMessage(json.dumps(data)):
+            self.writeText("Failed to delete project")
+        self._websocket.request(1)
 
 
 
@@ -86,10 +118,29 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
     ###### UI ######
     def getTabCaption(self):
         return "Highlight-Sharing"
+    def toggleScheme(self,var2):
+        self._scheme = not self._scheme
+    def toggleDomain(self,var2):
+        self._domain = not self._domain
+    def togglePath(self,var2):
+        self._path = not self._path
+    def toggleQuery(self,var2):
+        self._query = not self._query
 
     def getUiComponent(self):
         panel=JPanel()
 
+        scheme = JCheckBox("Scheme", actionPerformed= self.toggleScheme)
+        scheme.setSelected(True)
+        panel.add(scheme)
+        domain = JCheckBox("Domain", actionPerformed= self.toggleDomain)
+        domain.setSelected(True)
+        panel.add(domain)
+        path = JCheckBox("Path", actionPerformed= self.togglePath)
+        path.setSelected(True)
+        panel.add(path)
+        query = JCheckBox("Query", actionPerformed= self.toggleQuery)
+        panel.add(query)
         self._wsStringButton = JTextField('', 40)
         label = JLabel('WS String:', SwingConstants.RIGHT)
         panel.add(label)
@@ -100,20 +151,30 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
         panel.add(label)
         panel.add(self._userButton)
 
-        self._projectButton = JTextField('', 15)
         panel.add(JLabel("Project name:", SwingConstants.RIGHT))
-        panel.add(self._projectButton)
+        self._projectList = JList(self._projectNames)
+        panel.add(self._projectList)
 
+        projectButton = JButton("Select Project", actionPerformed=self.selectProject)
+        panel.add(projectButton)
+
+        deleteButton = JButton("Delete Project", actionPerformed=self.deleteProject)
+        panel.add(deleteButton)
+
+
+        self._newProject = JTextField('', 15)
+        label = JLabel('New project',SwingConstants.RIGHT)
+        panel.add(label)
+        panel.add(self._newProject)
+
+        button=JButton("Create project", actionPerformed=self.createProject)
+        panel.add(button)
 
         button=JButton("Connect", actionPerformed=self.connectWebsocket)
         panel.add(button)
 
         self._messageBox = JLabel("")
         panel.add(self._messageBox)
-
-
-        debug = JButton("Debug", actionPerformed=self.toggleDebug)
-        panel.add(debug)
 
         return panel
     ###### UI ######
@@ -122,12 +183,12 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
 
     def registerExtenderCallbacks(self,callbacks):
         self._callbacks=callbacks
+        self._callbacks.printOutput("Highlight-Sharing "+ self.version)
         self._helpers=callbacks.getHelpers()
         self._callbacks.setExtensionName("Highlight-Sharing")
         self._callbacks.addSuiteTab(self)
         self._callbacks.registerContextMenuFactory(self)
         self._callbacks.registerExtensionStateListener(self)
-        self._enabled=True
         self._connected=False
         self._requestIds=[]
         return
@@ -138,7 +199,7 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
             self._websocket.sendClose(1000, "Just closin")
 
 
-    def createMenuItems(self, invocation):
+    def createMenuItems(self, invocation): # to be removed :)
         ctx = invocation.getInvocationContext()
         ## Only works in http history and site map
         if ctx != invocation.CONTEXT_TARGET_SITE_MAP_TABLE and ctx != invocation.CONTEXT_TARGET_SITE_MAP_TREE and ctx != invocation.CONTEXT_PROXY_HISTORY:
@@ -146,25 +207,78 @@ class BurpExtender(IBurpExtender,IContextMenuFactory,ITab, IRequestInfo, IBurpEx
         for i in invocation.getSelectedMessages(): ## hacky hacky workaround... need to right click after all are highlighted
             color = i.getHighlight()
             if color:
+                url = i.getUrl().toString()
+                type = ""
+                value = ""
+                if self._scheme:
+                    type+="1"
+                    value+=url.split(":")[0]+"://"
+                else:
+                    value+="dummy://"
+                    type+="0"
+                if self._domain:
+                    value+=url.split("/")[2]
+                    type+="1"
+                else:
+                    value+="dummy"
+                    type+="0"
+                if self._path:
+                    value+="/"+"/".join(url.split("?")[0].split("/")[3:])
+                    type+="1"
+                else:
+                    value+="/"
+                    type+="0"
+                if self._query:
+                    value+="?"+"?".join(url.split("?")[1:])
+                    type+="1"
+                else:
+                    value+="?"
+                    type+="0"
                 comment=i.getComment()
-                path=i.getUrl().toString().split("?")[0]
-                data = {"user":self._user, "path":path, "color":color, "comment":comment, "project":self._projectName}
+                data = {"user":self._user, "type":type, "value":value, "color":color, "comment":comment, "projectname":self._projectName}
                 self.sendMessage(json.dumps(data))
         return None
 
-    def updateReq(self, path, color, comment):
+    def updateReq(self, type, value, color, comment):
         if self.debugging:
             self._callbacks.printOutput("Updating requests")
         reqs = self._callbacks.getSiteMap("")
+        scheme = ""
+        domain=""
+        path = ""
+        query = ""
+        if type[0] == "1":
+            scheme = value.split(":")[0] # need to verify that these values are correct
+        if type[1] == "1":
+            domain = value.split("/")[2]
+        if type[2] == "1":
+            path = "/"+"/".join(value.split("?")[0].split("/")[3:])
+        if type[3] == "1":
+            query = "?"+"?".join(value.split("?")[1:])
+
         for i in reqs:
+            scheme2 = ""
+            domain2 = ""
+            path2 = ""
+            query2 = ""
+            url = i.getUrl().toString()
+            if scheme != "":
+                scheme2 = url.split(":")[0]
+            if domain != "":
+                domain2 = url.split("/")[2]
+            if path != "":
+                path2 = "/"+"/".join(url.split("?")[0].split("/")[3:]) 
+            if query != "":
+                query2 = "?"+"?".join(url.split("?")[1:])
             url = i.getUrl().toString().split("?")[0]
-            if path == url:
-                i.setHighlight(color)
+            if scheme != scheme2 or domain != domain2 or path != path2 or query != query2:
+                continue
+            if self.debugging:
+                self._callbacks.printOutput("Setting color of " + url)
+            i.setHighlight(color)
+            if comment != "":
                 if self.debugging:
-                    self._callbacks.printOutput("Setting color of " + path)
-                if comment != "":
-                    if self.debugging:
-                        self._callbacks.printOutput("Setting comment of "+path)
-                    i.setComment(comment)
+                    self._callbacks.printOutput("Setting comment of "+url)
+                i.setComment(comment)
         if self.debugging:
             self._callbacks.printOutput("Updating requests done")
